45da360a4fdc1a98fec3f8ecdd2685fb526ba9b3
Elhabib Asmama <elhabibasmama@gmail.com>
2023-12-03 11:31:27 +0100

    Added method for parseing time units

--- TimeSeriesData/Functions/InfluxDbUtilities.cs
+++ TimeSeriesData/Functions/InfluxDbUtilities.cs
@@ -31,9 +31,19 @@ namespace Timeseriesdata.Functions
 
             if (DateTime.TryParse(timeString, out var normalDateTime))
                 return normalDateTime;
+            
 
             if (timeString.StartsWith('-') && char.IsDigit(timeString[1]))
             {
+                if (timeString.EndsWith("w") && int.TryParse(timeString.Substring(1, timeString.Length - 2), out var weeks))
+                    return DateTime.UtcNow.AddDays(-weeks * 7);
+
+                if (timeString.EndsWith("M") && int.TryParse(timeString.Substring(1, timeString.Length - 2), out var months))
+                    return DateTime.UtcNow.AddMonths(-months);
+
+                if (timeString.EndsWith("y") && int.TryParse(timeString.Substring(1, timeString.Length - 2), out var years))
+                    return DateTime.UtcNow.AddYears(-years);
+
                 if (timeString.EndsWith("h") && int.TryParse(timeString.Substring(1, timeString.Length - 2), out var hours))
                     return DateTime.UtcNow.AddHours(-hours);
 
@@ -42,9 +52,12 @@ namespace Timeseriesdata.Functions
 
                 if (timeString.EndsWith("m") && int.TryParse(timeString.Substring(1, timeString.Length - 2), out var minutes))
                     return DateTime.UtcNow.AddMinutes(-minutes);
-            }
 
-            // It can be extended for other time units like seconds, etc.
+                 if (timeString.EndsWith("s") && int.TryParse(timeString.Substring(1, timeString.Length - 2), out var seconds))
+                 return DateTime.UtcNow.AddSeconds(-seconds);
+
+                 // It can be extended for other time units.
+            }
 
             throw new ArgumentException($"Invalid time format: {timeString}");
         }
