4f4ad96ad6c37eb1adddc4356dfd789ee8f1d94b
Elhabib Asmama <elhabibasmama@gmail.com>
2023-12-03 11:17:03 +0100

    Clean up

--- TimeSeriesData/Program.cs
+++ TimeSeriesData/Program.cs
@@ -15,12 +15,10 @@ var settings = config.GetSection("InfluxDB").Get<InfluxDBSettings>();
 var influxDBClient = new InfluxDBClient(settings?.ServerUrl, settings?.Token);
 
 
-// Add services to the container.
-
 builder.Services.AddSingleton(influxDBClient);
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -28,7 +26,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
