46bec2f0f7886d897188aefac5d7370ae527522d
Elhabib Asmama <elhabibasmama@gmail.com>
2023-12-05 19:53:23 +0100

    Merge branch 'dev' into feature/StartProject

--- TimeSeriesData/Program.cs
+++ TimeSeriesData/Program.cs
@@ -1,4 +1,6 @@
 using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,7 +21,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Our Services
+//builder.Services.AddScoped<IProductsService, ProductsService>();
 builder.Services.AddScoped<IInfluxWriterService, InfluxWriterService>();
 builder.Services.AddScoped<IInfluxFetcherService, InfluxFetcherService>();
 
